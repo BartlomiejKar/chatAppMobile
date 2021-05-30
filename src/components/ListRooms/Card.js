9032bead3a2865d3fb4f673f7c98ddb2bc2ad778
Bartek Karelus <bartek.karelus@gmail.com>
2021-05-30 18:34:36 +0200

    add last messages nad display

--- src/components/ListRooms/Card.js
+++ src/components/ListRooms/Card.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProfileIcon from "../../../assets/profile"
+import { useQuery } from "@apollo/client";
+
 
 import { View, Text, StyleSheet, Image } from "react-native"
 
+import { MessagesRoom } from "../utils/data"
+
 
 const Card = ({ rooms }) => {
+    const [messages, setMessages] = useState([])
     const { name, roomPic } = rooms;
-    console.log(rooms)
+    const { data } = useQuery(MessagesRoom, {
+        variables: { id: rooms.id }
+    })
+    const messagesArray = messages.map(el => {
+        return el.text
+    })
+    useEffect(() => {
+        if (data) {
+            const messagesInRoom = data?.room?.messages.map(el => {
+                let messages = {
+                    _id: el.id,
+                    text: el.body,
+                }
+                return messages
+            })
+            setMessages(messagesInRoom)
+        }
+
+    }, [data])
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
                 {roomPic ? <Image source={{ uri: roomPic }} style={{ width: 64, height: 64, borderRadius: 100, backgroundColor: "blue" }} /> : <ProfileIcon />}
             </View>
             <View style={styles.messages}>
-                <Text>{name}</Text>
-                <Text>Some messages</Text>
+                <Text style={styles.titleMessages}>{name}</Text>
+                <Text>{messagesArray[messagesArray.length - 1]}</Text>
             </View>
         </View >
 
@@ -31,10 +54,13 @@ const styles = StyleSheet.create({
         marginBottom: 12
     },
     messages: {
-        justifyContent: "center"
+        justifyContent: "center",
+        padding: 5
     },
     imageContainer: {
         margin: 16,
+    },
+    titleMessages: {
     }
 })
 export default Card
